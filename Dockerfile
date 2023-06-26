537c36283b58e7ade64029fce329f506e24c20d5
Phil Willis <philopian@gmail.com>
2023-06-25 22:31:34 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3
+
+WORKDIR /app
+
+COPY hello.py .
+
+CMD ["python", "hello.py"]
---
+++ hello.py
@@ -0,0 +1,4 @@
+import os
+
+my_env_variable = os.getenv("MY_ENV_VARIABLE")
+print("MY_ENV_VARIABLE.......", my_env_variable)
